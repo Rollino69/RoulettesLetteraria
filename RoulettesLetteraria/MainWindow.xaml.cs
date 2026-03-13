51b04bc7a2a6e57932047ddda9b004367027a8cd
Federico Minotti <FEDERICO.MINOTTI@local.ittcesena.it>
2026-03-13 09:20:19 +0100

    R5.0.0: Logic improved and runnable

--- RoulettesLetteraria/MainWindow.xaml.cs
+++ RoulettesLetteraria/MainWindow.xaml.cs
@@ -25,55 +25,86 @@ namespace RoulettesLetteraria
             InitializeComponent();
         }
 
-       /* public void InseireLunghezza()
-        {
-            MessageBox.Show("inserire la lunghezza dderata della parola"); //pag 18 classi terze
-            string lunghezza = txt_lunghezza.Text;
-             if (int.TryParse(string lunghezza out int result))
-             {
-                 if (v > 0)
-                 {
-                    
-                     MessageBox.Show($"Lunghezza inserita: {v}");
-                 }
-                 else
-                 {
-                     MessageBox.Show("La lunghezza deve essere un numero positivo.");
-                 }
-             }
-             else
-             {
-                 MessageBox.Show("Per favore, inserisci un numero valido per la lunghezza.");
+        /* public void InseireLunghezza()
+         {
+             MessageBox.Show("inserire la lunghezza dderata della parola"); //pag 18 classi terze
+             string lunghezza = txt_lunghezza.Text;
+              if (int.TryParse(string lunghezza out int result))
+              {
+                  if (v > 0)
+                  {
 
-            }*/
-       
-            
+                      MessageBox.Show($"Lunghezza inserita: {v}");
+                  }
+                  else
+                  {
+                      MessageBox.Show("La lunghezza deve essere un numero positivo.");
+                  }
+              }
+              else
+              {
+                  MessageBox.Show("Per favore, inserisci un numero valido per la lunghezza.");
+
+             }*/
+
+
 
 
         private async void btn_stampa_Click(object sender, RoutedEventArgs e)
         {
-            if(btn_stampa.Content.ToString() == "Inizia")
+            
+            int limiteMassimo = 0;
+            int.TryParse(txt_lunghezza.Text, out limiteMassimo);
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            if (btn_stampa.Content.ToString() == "Inizia")
             {
                 btn_stampa.Content = "Stampa";
 
-                string lunghezza = txt_lunghezza.Text;
-
-                if (int.TryParse(txt_lunghezza.Text, out int limiteMassimo))
-                {
-                    // Usa 'numero' per la tua logica
-                }
+                
                 while (true)
                 {
-                    lbl_stampa.Content = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Z" }[new Random().Next(0, 21)];
+                    string[] lettere = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Z" };
+                    lbl_stampa.Content = lettere[new Random().Next(0, lettere.Length)]; //come massimo metti la lunghezz ainseritsa
+                    
+                    
                     await Task.Delay(100);
                 }
             }
-            if else(lsb_parole.Items.Count <  )
+            
+            else if (lsb_parole.Items.Count < limiteMassimo)
             {
                 
                 
-                lsb_parole.Items.Add(new ListBoxItem() { Content = lbl_stampa.Content.ToString() });
                 
+                
+                lsb_parole.Items.Add(lbl_stampa.Content.ToString());
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            }
+            else
+            {
+                MessageBox.Show("l max");
             }
         }
     }
