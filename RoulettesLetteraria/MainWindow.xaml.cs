7428fdefa31a7c367c8b3aa9c5ea27a311a073e5
Federico Minotti <FEDERICO.MINOTTI@local.ittcesena.it>
2026-03-13 09:08:59 +0100

    R4.0.0: Insert the lenght of the word

--- RoulettesLetteraria/MainWindow.xaml.cs
+++ RoulettesLetteraria/MainWindow.xaml.cs
@@ -25,11 +25,28 @@ namespace RoulettesLetteraria
             InitializeComponent();
         }
 
-        public void InseireLunghezza()
+       /* public void InseireLunghezza()
         {
-            MessageBox.Show("inserire la lunghezza dderata della parola");
+            MessageBox.Show("inserire la lunghezza dderata della parola"); //pag 18 classi terze
+            string lunghezza = txt_lunghezza.Text;
+             if (int.TryParse(string lunghezza out int result))
+             {
+                 if (v > 0)
+                 {
+                    
+                     MessageBox.Show($"Lunghezza inserita: {v}");
+                 }
+                 else
+                 {
+                     MessageBox.Show("La lunghezza deve essere un numero positivo.");
+                 }
+             }
+             else
+             {
+                 MessageBox.Show("Per favore, inserisci un numero valido per la lunghezza.");
 
-        }
+            }*/
+       
             
 
 
@@ -38,15 +55,20 @@ namespace RoulettesLetteraria
             if(btn_stampa.Content.ToString() == "Inizia")
             {
                 btn_stampa.Content = "Stampa";
-            
 
+                string lunghezza = txt_lunghezza.Text;
+
+                if (int.TryParse(txt_lunghezza.Text, out int limiteMassimo))
+                {
+                    // Usa 'numero' per la tua logica
+                }
                 while (true)
                 {
                     lbl_stampa.Content = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Z" }[new Random().Next(0, 21)];
                     await Task.Delay(100);
                 }
             }
-            if else(lsb_parole.Items.Count < )
+            if else(lsb_parole.Items.Count <  )
             {
                 
                 
